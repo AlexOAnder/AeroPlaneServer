d53066ce47c20ac0c31dbd0c9a27b7f6e5876e64
AlexOAnder <thanksforsource@gmail.com>
2016-01-08 21:53:52 +0300

    fix

--- src/UDPAeroServer.java
+++ src/UDPAeroServer.java
@@ -1,6 +1,8 @@
+import java.io.FileInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Properties;
 
 public class UDPAeroServer {
 	public static void main(String args[]) throws Exception
@@ -8,6 +10,7 @@ public class UDPAeroServer {
        DatagramSocket serverSocket = new DatagramSocket(5022);
           byte[] receiveData = new byte[1024];
           byte[] sendData = new byte[1024];
+          System.out.println("Hi - its a AeroPlane Server!");
           while(true)
              {
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
@@ -25,4 +28,18 @@ public class UDPAeroServer {
                 serverSocket.send(sendPacket);
              }
     }
+	
+	private void LoadPlaneData()
+	{
+		FileInputStream fis;
+        Properties property = new Properties();
+        
+		fis = new FileInputStream("src/PlaneData.txt");
+        // load config.sys from stream
+        property.load(fis);
+
+        uri = property.getProperty("db.uri");
+        login = property.getProperty("db.login");
+        pass = property.getProperty("db.password");
+	}
 }
